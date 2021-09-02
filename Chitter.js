f233a9821203112ad3a464c80ff2d891e05bc8d1
FUN N GAMES with Arpit <75426914+Ahttp@users.noreply.github.com>
2021-09-02 14:56:29 +0530

    Add files via upload

---
+++ Chitter.js
@@ -0,0 +1,24 @@
+function addUser() {
+    user_name = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "Chitter_room.html";
+}
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+    Room_names = childKey;
+    console.log("Room Name - " + Room_names);
+   console.log(firebase_message_id);
+      console.log(message_data);
+      name = message_data['name'];
+      message = message_data['message'];
+         like = message_data['like'];
+         name_with_tag = "<h4> "+ name +"<img class='user_tick' src='tick.png'></h4>";
+         message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
+like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
+         span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
+
+        row = name_with_tag + message_with_tag +like_button + span_with_tag;      
+        document.getElementById("output").innerHTML += row;
+ });
+});
